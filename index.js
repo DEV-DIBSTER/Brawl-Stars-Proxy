faf35a415ccbac841c757cd81e1a7f2bd550677d
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-07-07 17:49:52 -0400

    feat: setting up faster https proxy

--- index.js
+++ index.js
@@ -2,39 +2,35 @@ const ExpressJS = require('express');
 const BodyParser = require('body-parser');
 const Chalk = require('chalk');
 const Axios = require('axios');
+const HTTPsProxyMiddleware = require("http-proxy-middleware");
 const Exec = require('child_process').exec;
 
 const Configuration = require('./config.json');
 
 const Server = ExpressJS();
+
+const BrawlStarsProxy = HTTPsProxyMiddleware.createProxyMiddleware({
+    target: "https://api.brawlstars.com",
+    changeOrigin: true,
+    logLevel: "silent",
+    pathRewrite: {
+      "^/v1": "/v1",
+    },
+    onProxyReq: async (ProxyRequest, Request, Response) => {
+        const userIP = (Request.headers["cf-connecting-ip"] || Request.headers["x-forwarded-for"] || Request.ip).replace(/^::ffff:/, "");
+        console.log(`${Chalk.greenBright(`[Server] | `)}${Chalk.bold.blueBright(`Request made from ${userIP} to ${Request.hostname}${Request.originalUrl}.`)}`);
+
+        if(Request.hostname != Configuration.URL) return await Response.status(403).send('Unauthorized Domain.');
+        if(!Request.headers.authorization) return await Response.status(404).send('Missing data values.');
+
+        ProxyRequest.setHeader("Authorization", `Bearer ${Request.headers.authorization}`);
+    },
+});
+
 Server.set('json spaces', 2);
 Server.use(BodyParser.json(), BodyParser.urlencoded({extended: true}));
 
-Server.get('/v1', async (Request, Response) => {
-    Response.status(200).send('Version: 1');
-});
-
-Server.get('*', async (Request, Response) => {
-    //Logs the request.
-	const userIP = (Request.headers["cf-connecting-ip"] || Request.headers["x-forwarded-for"] || Request.ip).replace(/^::ffff:/, "");
-    console.log(`${Chalk.greenBright(`[Server] | `)}${Chalk.bold.blueBright(`Request made from ${userIP} to ${Request.hostname}${Request.originalUrl}.`)}`);
-
-    if(Request.hostname != Configuration.URL) return await Response.status(403).send('Unauthorized Domain.');
-    if(!Request.headers.authorization) return await Response.status(404).send('Missing data values.');
-
-    await Axios({
-        url: `https://api.brawlstars.com${Request.url}`,
-        method: 'GET',
-        headers: {
-            'Authorization': `${Request.headers.authorization}`,
-            'Content-Type': 'application/json'
-        }
-    }).then(BrawlStarsResponse => {
-        Response.status(BrawlStarsResponse.status).send(BrawlStarsResponse.data);
-    }).catch(Error => {
-        Response.status(Error.toJSON().status).send(`${Error.toJSON().message}`);
-    });
-});
+Server.use('/v1', BrawlStarsProxy);
 
 setInterval(() => {
 	if(Configuration.AutomaticUpdate == false) return;
